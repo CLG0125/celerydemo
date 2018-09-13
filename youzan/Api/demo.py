83663aad07490c666c745bec3810d71a71f1ff7a
陈罗广 <clg103@163.com>
2018-09-13 18:13:28 +0800

    增加一些功能

--- youzan/Api/demo.py
+++ youzan/Api/demo.py
@@ -1,12 +1,44 @@
+import random
+
+from django.contrib.auth.models import User
 from django.http.response import HttpResponse
 from Api.resources import Rest
 import json
 
+from interapp.models import UC
 
 
 class Demo(Rest):
     def get(self, request, *args, **kwargs):
-        pass
+        data = request.GET
+        username = data.get("username", "")
+        password = data.get("password", "")
+        entu_password = data.get("entu_password", "")
+        category = data.get("category", "")
+        if password != entu_password:
+            data = {
+                "msg": "两次密码要一致"
+            }
+            return HttpResponse(json.dumps(data), content_type="application/json", status=200)
+        elif not username:
+            data = {
+                "msg": "用户名不允许为空"
+            }
+            return HttpResponse(json.dumps(data), content_type="application/json", status=200)
+        user = User()
+        user.username = username
+        user.set_password(password)
+        user.save()
+        if category == "customer":
+            uc = UC()
+            uc.user = user
+            uc.stack_ID = "9527"
+            uc.save()
+        data_return = {
+            "msg": "ok"
+        }
+        return HttpResponse(json.dumps(data_return), content_type="application/json", status=200)
 
-    def post(self, request, *args, **kwargs):
-        pass
+
+def post(self, request, *args, **kwargs):
+    return HttpResponse(json.dumps("name"), content_type="application/json", status=200)
--- youzan/interapp/models.py
+++ youzan/interapp/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class UC(models.Model):
+    user = models.OneToOneField(User)
+    stack_ID = models.CharField(default="", help_text="股票编号", max_length=64)
