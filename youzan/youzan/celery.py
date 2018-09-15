a079dc85f656c109713264e1d4201a74836d49b6
CLG0125 <clg103@163.com>
2018-09-15 16:54:01 +0800

    增加route功能用于配置多任务优先级

--- youzan/youzan/celery.py
+++ youzan/youzan/celery.py
@@ -14,3 +14,17 @@ def debug_task(self):
     print("name is ....")
 
 # celery -A youzan worker -l info -P eventlet
+
+
+from kombu import Exchange, Queue
+
+default_exchange = Exchange('default', type='direct')
+media_exchange = Exchange('media', type='direct')
+app.conf.task_queues = {
+    Queue('default', default_exchange, routing_key='default'),
+    Queue('video', media_exchange, routing_key='media.video'),
+    Queue('image', media_exchange, routing_key='media.image')
+}
+app.conf.task_default_queue = 'default'
+app.conf.task_default_exchange = 'default'
+app.conf.task_default_routing_key = 'default'
