3a72e81defe718316b8735726de96ef0397ee295
陈罗广 <clg103@163.com>
2018-09-25 16:03:10 +0800

    增加一些功能和注释

--- youzan/youzan/celery.py
+++ youzan/youzan/celery.py
@@ -13,9 +13,11 @@ app.autodiscover_tasks()
 def debug_task(self):
     print("name is ....")
 
+
 # celery -A youzan worker -l info -P eventlet
 
 
+# 多路由处理不同的任务
 from kombu import Exchange, Queue
 
 default_exchange = Exchange('default', type='direct')
@@ -28,3 +30,20 @@ app.conf.task_queues = {
 app.conf.task_default_queue = 'default'
 app.conf.task_default_exchange = 'default'
 app.conf.task_default_routing_key = 'default'
+
+# 调用任务时,不但需要传参并且对任务的重试次数已经重试间隔时间有其他要求的时候,delay()已经不适用了,需要用apply_async()的方式进行处理
+# 例如,现在需要将task中的add函数进行异步引用操作
+from interapp.tasks import add
+
+# 注意在传参数的时候不能像delay()一样直接进行传参,需要将位置参数放进[ ]列表中  关键字参数放进{ }字典中
+# 这样是为了区别于后面的设置参数
+add.apply_async([1, 3, 4], {"token": 123}, retry=True, retry_policy={
+    # 最大的重试次数
+    'max_retries': 3,
+    # 定义首次重试间隔的秒数
+    'interval_start': 0,
+    # 每进行一次重试,延迟的时间长度
+    'interval_step': 0.2,
+    # 重试之间间隔的最大秒数
+    'interval_max': 0.2,
+})
