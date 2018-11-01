b50706fe7ad510e8c66a5c18c499b097c5a4f0d6
陈罗广 <clg103@163.com>
2018-11-01 20:08:07 +0800

    update

--- djlog/__init__.py
+++ djlog/__init__.py
@@ -1,3 +1,7 @@
 import os
 
 log_dir = os.path.dirname(os.path.abspath(__file__))
+# dshfkjhdsafkjdsahfkjdsahfkjdshfkjdsahfjkdsaf
+# sdfkjhsakjdhfkjdshfkjdsahfkjdsahfkjdsahfkjds
+# hdsfkhdskjdsahfdsjhafhdsakjhfkjdsa
+# 这是公司的电脑
