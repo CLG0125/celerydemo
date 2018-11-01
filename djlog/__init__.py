524e1644fffe3fe189b1da4eae71f878c9e8c338
陈罗广 <clg103@163.com>
2018-11-01 20:33:05 +0800

    uodate

--- djlog/__init__.py
+++ djlog/__init__.py
@@ -1,7 +1,3 @@
 import os
 
 log_dir = os.path.dirname(os.path.abspath(__file__))
-# dshfkjhdsafkjdsahfkjdsahfkjdshfkjdsahfjkdsaf
-# sdfkjhsakjdhfkjdshfkjdsahfkjdsahfkjdsahfkjds
-# hdsfkhdskjdsahfdsjhafhdsakjhfkjdsa
-# 这是公司的电脑
