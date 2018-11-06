689ade0b24d704f3e31e3adcb0329beb2e2f1261
陈罗广 <clg103@163.com>
2018-11-06 15:20:47 +0800

    updat

--- youzan/interapp/tests.py
+++ youzan/interapp/tests.py
@@ -1,15 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-# 这是dev
-# 这是dev
-# 这个就是dev的东西
-# 这个完全是dev的东西
-# 这是准备删除的东西合并的时候
-# 这是master分支的东西
-# 这是master分支的东西
-# 这个还是master的东西
-# 这个还是master的东西
-# 这个是准备合并到dev分支的东西
-# OOOOOOOO
-# 啊哈哈
